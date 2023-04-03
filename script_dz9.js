56ff0d0bb88cd34f71b0eced1a50004ef0001679
Никита Баранов <124291854+Borbos5@users.noreply.github.com>
2023-04-03 22:49:14 +0300

    Add files via upload

---
+++ script_dz9.js
@@ -0,0 +1,19 @@
+//1
+const car = {
+    brand: 'BMW',
+    model: 'X5',
+    year: 2020,
+    color: 'black',
+    engine: {
+        type: 'V8',
+        volume: 4.4
+    },
+};
+
+//2
+const student = {
+    firstName: 'Nikita',
+    lastName: 'Branov',
+    age: 14,
+    school: '72',
+};
